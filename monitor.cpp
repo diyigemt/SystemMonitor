a371d1001b96bf180a0508e34d5c55909b00cabf
unknown <cecilwyx@qq.com>
2020-04-15 21:10:31 +0800

    Complete Monitor version 1.

--- monitor.cpp
+++ monitor.cpp
@@ -1,44 +1,135 @@
 #pragma    comment(lib,"Pdh.lib")
 #include "monitor.h"
+#include <PdhMsg.h>
 
-#include <QString.h>
-#include <Pdh.h>
-#include <pdhmsg.h>
-#include <Windows.h>
 
+/**
+ * @brief Monitor::Monitor 无参构造函数，默认构建四个计数器
+ */
 Monitor::Monitor()
 {
-
+    PDH_STATUS status = PdhOpenQuery(NULL, NULL, &this->m_hQuery);
+    if (status != ERROR_SUCCESS)
+    {
+        MessageBox(NULL, TEXT("初始化查询失败！"), TEXT(""), MB_OK);
+    }
+    // 添加各种Counter
+    status = PdhAddCounter(this->m_hQuery, TEXT("\\Processor Information(_Total)\\% Processor Utility"), NULL, &this->m_hTotalCPUCounter);
+    status = PdhAddCounter(this->m_hQuery, TEXT("\\Process(_Total)\\Working Set - Private"), NULL, &this->m_hTotalMemoryCounter);
+    status = PdhAddCounter(this->m_hQuery, TEXT("\\Process(_Total)\\IO Read Bytes/sec"), NULL, &this->m_hTotalDiskReadCounter);
+    status = PdhAddCounter(this->m_hQuery, TEXT("\\Process(_Total)\\IO Write Bytes/sec"), NULL, &this->m_hTotalDiskWriteCounter);
 }
+
+
+/**
+ * @brief Monitor::~Monitor 析构函数，在这里移除counter，关闭查询
+ */
 Monitor::~Monitor()
 {
-
+   PdhRemoveCounter(this->m_hTotalCPUCounter);
+   PdhRemoveCounter(this->m_hTotalMemoryCounter);
+   PdhRemoveCounter(this->m_hTotalDiskReadCounter);
+   PdhRemoveCounter(this->m_hTotalDiskWriteCounter);
+   PdhCloseQuery(this->m_hQuery);
 }
-QString Monitor::getCPUUsage()
+
+/**
+ * @brief Monitor::GetMemoryUsage 获得所有进程的内存使用率
+ * @return double 总体内存使用率
+ */
+double Monitor::GetMemoryUsage()
 {
-    HQUERY query;
-    PDH_STATUS status = PdhOpenQuery(NULL, NULL, &query);
-    PDH_STATUS AvilableMemorystatus = PdhOpenQuery(NULL, NULL, &query);
+    return this->m_dbTotalMemoryUsage;
+}
 
-    if (status != ERROR_SUCCESS)
-        return "Open Query Error";
+/**
+ * @brief Monitor::GetCPUUsage 获得所有进程的CPU使用率
+ * @return double 总体CPU使用率
+ */
+double Monitor::GetCPUUsage()
+{
+    return this->m_dbTotalCPUUsage;
+}
 
-    HCOUNTER counter;
 
-    //counter = (HCOUNTER*)GlobalAlloc(GPTR, sizeof(HCOUNTER));
-    HCOUNTER cntProcessCPU, cntAvilableMemory;
-    status = PdhAddCounter(query, TEXT("\\Processor Information(_Total)\\% Processor Utility"), NULL, &counter);
+/**
+ * @brief Monitor::GetDiskReadSpeed 获得总体的磁盘读取速度
+ * @return double 总体的磁盘读取速度
+ */
+double Monitor::GetDiskReadSpeed()
+{
+    return this->m_dbTotalDiskReadSpeed;
+}
 
-    PdhCollectQueryData(query);
-    Sleep(1000);
-    PdhCollectQueryData(query);
 
-    PDH_FMT_COUNTERVALUE pdhValue;
-    DWORD dwValue;
+/**
+ * @brief Monitor::GetDiskWriteSpeed 获得总体的磁盘写入速度
+ * @return double 总体的磁盘写入速度
+ */
+double Monitor::GetDiskWriteSpeed()
+{
+    return this->m_dbTotalDiskWriteSpeed;
+}
 
-    status = PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, &dwValue, &pdhValue);
-    if (status != ERROR_SUCCESS)
-        return  "Get Value Error";
-    QString s = "processCPU:" + QString("%1").arg(pdhValue.doubleValue);
-    return s;
+/**
+ * @brief Monitor::Update 这是一个私有函数，用来更新Monitor的各种counter的数据，可以作为一个线程定时更新，也可以实时调用。
+ * @return int status 返回状态值
+ */
+int Monitor::Update()
+{
+    PDH_STATUS status;
+    status = PdhCollectQueryData(this->m_hQuery);
+    Sleep(1000);  // 睡眠一秒钟再次收集一次数据
+    status = PdhCollectQueryData(this->m_hQuery);
+    if (ERROR_SUCCESS != status)
+    {
+        MessageBox(NULL, TEXT("收集数据失败！"), TEXT(""), MB_OK);
+        return -1;
+    }
+
+    // 格式化数据
+    status = PdhGetFormattedCounterValue(this->m_hTotalCPUCounter, PDH_FMT_DOUBLE, &this->m_dwValue, &this->m_pdhCounterValue);
+    if (ERROR_SUCCESS != status)
+    {
+        MessageBox(NULL, TEXT("格式化CPU使用率失败！"), TEXT(""), MB_OK);
+        return -1;
+    }
+    else
+    {
+        this->m_dbTotalCPUUsage = this->m_pdhCounterValue.doubleValue;
+    }
+
+    status = PdhGetFormattedCounterValue(this->m_hTotalMemoryCounter, PDH_FMT_DOUBLE, &this->m_dwValue, &this->m_pdhCounterValue);
+    if (ERROR_SUCCESS != status)
+    {
+        MessageBox(NULL, TEXT("格式化内存使用率失败！"), TEXT(""), MB_OK);
+        return -1;
+    }
+    else
+    {
+        this->m_dbTotalMemoryUsage = this->m_pdhCounterValue.doubleValue;
+    }
+
+    status = PdhGetFormattedCounterValue(this->m_hTotalDiskReadCounter, PDH_FMT_DOUBLE, &this->m_dwValue, &this->m_pdhCounterValue);
+    if (ERROR_SUCCESS != status)
+    {
+        MessageBox(NULL, TEXT("格式化磁盘读取速度失败！"), TEXT(""), MB_OK);
+        return -1;
+    }
+    else
+    {
+        this->m_dbTotalDiskReadSpeed = this->m_pdhCounterValue.doubleValue;
+    }
+
+    status = PdhGetFormattedCounterValue(this->m_hTotalDiskWriteCounter, PDH_FMT_DOUBLE, &this->m_dwValue, &this->m_pdhCounterValue);
+    if (ERROR_SUCCESS != status)
+    {
+        MessageBox(NULL, TEXT("格式化磁盘写入速度失败！"), TEXT(""), MB_OK);
+        return -1;
+    }
+    else
+    {
+        this->m_dbTotalDiskWriteSpeed = this->m_pdhCounterValue.doubleValue;
+    }
+    return 0;
 }
--- monitor.h
+++ monitor.h
@@ -2,12 +2,35 @@
 #define MONITOR_H
 
 #include <QString>
+#include <Windows.h>
+#include <Pdh.h>
+
 class Monitor
 {
 public:
     Monitor();
     ~Monitor();
-    QString getCPUUsage();
+    double GetCPUUsage();
+    double GetMemoryUsage();
+    double GetDiskReadSpeed();
+    double GetDiskWriteSpeed();
+private:
+    double m_dbTotalCPUUsage;
+    double m_dbTotalMemoryUsage;
+    double m_dbTotalDiskReadSpeed;
+    double m_dbTotalDiskWriteSpeed;
+
+private:
+    void Update();
+
+private:
+    HCOUNTER m_hTotalCPUCounter;
+    HCOUNTER m_hTotalMemoryCounter;
+    HCOUNTER m_hTotalDiskReadCounter;
+    HCOUNTER m_hTotalDiskWriteCounter;
+    DWORD m_dwValue;
+    PDH_FMT_COUNTERVALUE m_pdhCounterValue;
+    HQUERY m_hQuery;
 };
 
 #endif // MONITOR_H
